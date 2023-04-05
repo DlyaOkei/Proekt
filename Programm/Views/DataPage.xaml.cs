500729c8f8017ee74ed2bd40376e90a9c1c812d9
IBesteI <olimpiady.dlya@mail.ru>
2023-04-05 09:11:48 +0500

    Добавьте файлы проекта.

---
+++ Programm/Views/DataPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Programm.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programm.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DataPage.xaml
+    /// </summary>
+    public partial class DataPage : Page
+    {
+        public DataPage()
+        {
+            InitializeComponent();
+            Loaded += SignUpPage_Loaded;
+        }
+
+        private void SignUpPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Height = 720;
+            Window.GetWindow(this).Width = 1080;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            UsersGrid.ItemsSource = AppData.db.Client.ToList();
+        }
+
+        private void BtnData2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DataPage2());
+        }
+        private void RemoveBtn_click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите удалить пользователя", "уведомление", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                var CurrentClient = UsersGrid.SelectedItem as Client;
+                AppData.db.Client.Remove(CurrentClient);
+                AppData.db.SaveChanges();
+                UsersGrid.ItemsSource = AppData.db.Client.ToList();
+                MessageBox.Show("Success");
+            }
+
+        }
+    }
+}
---
+++ Programm/Views/DataPage2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Programm.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programm.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DataPage2.xaml
+    /// </summary>
+    public partial class DataPage2 : Page
+    {
+        public DataPage2()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            UsersGrid.ItemsSource = AppData.db.Client.ToList();
+        }
+
+        private void BtnData1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DataPage());
+        }
+
+        
+    }
+}
